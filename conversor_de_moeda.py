866608b4d5a5d4b0ab7c8f7ede3bcdf21d8ee312
Guilherme Silva <105737486+Scottzera1@users.noreply.github.com>
2023-05-21 19:29:26 -0300

    Add files via upload

---
+++ conversor_de_moeda.py
@@ -0,0 +1,5 @@
+dindin = float(input("Digite o valor que você tem na carteira? "))
+
+Doleta = dindin/ 5.00
+
+print("Com o tanto que você tem R$ {:.2f} em reais\nVocê ter em US$ {:.2f} de dolar para gastar".format(dindin, Doleta))
