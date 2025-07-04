04ff2b5c8a94bf470cc9a9260020747deeb8005e
Guilherme Silva <105737486+Scottzera1@users.noreply.github.com>
2025-07-03 23:08:50 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+nome = input ("Digite seu nome: ")
+# nome = "Guilherme sugestao"
+idade = int(input("Digite sua idade: "))
+# idade = ??
+peso = float(input("Digite seu peso: "))
+# peso = ??.?
+print(nome)
+print(idade)
+print(peso)
