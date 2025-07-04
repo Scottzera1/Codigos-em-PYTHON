f91ecc01a96dd916c50a1833784919d77da2ddab
Guilherme Silva <105737486+Scottzera1@users.noreply.github.com>
2025-07-03 23:08:00 -0300

    Add files via upload

---
+++ entrada_balada.py
@@ -0,0 +1,9 @@
+#como podemos ver se tal pessoa pode entrar em uma balada
+idade = int(input("Digite sua idade: "))
+# idade = 18
+
+if idade >=18:
+  print("Pode entrar")
+
+else:
+    print("Não pode entrar")
